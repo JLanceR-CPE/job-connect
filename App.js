3acf891bcba5a3b8338fedfc31b7bdf2f97de6a1
JLanceR-CPE <qjlpronquillo@tip.edu.ph>
2023-11-28 01:35:06 +0800

    added flash message

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AuthStack from './stack/AuthStack.js';
 import AppStack from './stack/AppStack.js';
 
+import FlashMessage from "react-native-flash-message";
+
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
+      <FlashMessage position="top" />
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Auth" component={AuthStack} />
         <Stack.Screen name="App" component={AppStack} />
